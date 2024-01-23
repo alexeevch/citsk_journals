b004eb934c6bf63525c3a7766636f23efe7b744d
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-01-23 15:49:17 +0300

    feat: Фабрики и сидеры для IncidentType и IncidentStatus

---
+++ backend/database/seeders/IncidentStatusSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\IncidentStatus;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class IncidentStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            "Новая",
+            "Закрыта"
+        ];
+
+        IncidentStatus::factory()
+            ->count(count($statuses))
+            ->sequence(fn(Sequence $sequence) => ['name' => $statuses[$sequence->index]])
+            ->create();
+    }
+}
---
+++ backend/database/seeders/IncidentTypeSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\IncidentType;
+use Illuminate\Database\Seeder;
+
+class IncidentTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        IncidentType::factory()->create([
+            'name' => 'DDOS',
+            'description' => 'DDoS-атака (или Denial of Service, или «отказ в обслуживании») — это попытка злоумышленников так загрузить сервер, чтобы он просто перестал работать.'
+        ]);
+    }
+}
