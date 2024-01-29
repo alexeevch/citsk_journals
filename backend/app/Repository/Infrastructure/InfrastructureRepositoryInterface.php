5256bfb6a52d44455ca0cc3100281cf93288ee26
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-01-29 16:01:23 +0300

    feat: Реализован InfrastructureRepository

---
+++ backend/app/Repository/Infrastructure/InfrastructureRepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository\Infrastructure;
+
+use App\DTO\Infrastructure\InfrastructureCreateDTO;
+use App\DTO\Infrastructure\InfrastructureUpdateDTO;
+use App\Models\Infrastructure;
+
+interface InfrastructureRepositoryInterface
+{
+    /**
+     * @param  InfrastructureCreateDTO  $infrastructureCreateDTO
+     *
+     * @return Infrastructure
+     */
+    function create(InfrastructureCreateDTO $infrastructureCreateDTO): Infrastructure;
+
+    /**
+     * @param  InfrastructureUpdateDTO  $infrastructureUpdateDTO
+     *
+     * @return Infrastructure
+     */
+    function update(InfrastructureUpdateDTO $infrastructureUpdateDTO): Infrastructure;
+}
