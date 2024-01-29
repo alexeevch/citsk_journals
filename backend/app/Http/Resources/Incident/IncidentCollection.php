eef180da864a16159940f832c0d794edc1a59c32
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-01-29 14:36:52 +0300

    chore: Заготовка IncidentCollection

--- backend/app/Http/Resources/Incident/IncidentCollection.php
+++ backend/app/Http/Resources/Incident/IncidentCollection.php
@@ -14,6 +14,8 @@ class IncidentCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+        ];
     }
 }
