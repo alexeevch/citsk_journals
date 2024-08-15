1edec21532839654ce420e997637da2b3754a341
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-08-15 16:53:06 +0300

    fix(DatabaseSeeder.php): Удалены неиспользуемые переменные

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -6,13 +6,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $incidentType = $this->call(IncidentTypeSeeder::class);
-        $incidentStatus = $this->call(IncidentStatusSeeder::class);
-        $incident = $this->call(IncidentSeeder::class);
+        $this->call(IncidentTypeSeeder::class);
+        $this->call(IncidentStatusSeeder::class);
+        $this->call(IncidentSeeder::class);
     }
 }
