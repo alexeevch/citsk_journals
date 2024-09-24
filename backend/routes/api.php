b4cede265e1f36619cd89912e9c5dae08436eba7
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-09-24 23:32:20 +0300

    feat: Пробросил маршрут для регистрации

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\IncidentController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("v1")->group(function () {
+    Route::post("/auth/register", [AuthController::class, "register"]);
+
     Route::apiResource("/incident", IncidentController::class);
 });
