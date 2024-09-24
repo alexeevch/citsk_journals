16c831670f411d159eace3ed95f5f249392d1008
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-09-24 21:50:37 +0300

    feat: Класс с константами

---
+++ backend/app/Constants.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+class Constants
+{
+    /*
+    |--------------------------------------------------------------------------
+    | MESSAGES
+    |--------------------------------------------------------------------------
+     */
+
+    public const USER_UNAUTHORIZED_MESSAGE = 'Unauthorized';
+    public const USER_UNAUTHENTICATED_MESSAGE = "Unauthenticated";
+    public const NOT_FOUND_MESSAGE = "Not found";
+    public const DUPLICATE_ENTRY_MESSAGE = "Duplicate entry";
+
+    /*
+    |--------------------------------------------------------------------------
+    | ERROR CODES
+    |--------------------------------------------------------------------------
+     */
+
+    public const DUPLUCATE_ENTRY_CODE = 1062;
+
+    /*
+    |--------------------------------------------------------------------------
+    | ROLES
+    |--------------------------------------------------------------------------
+     */
+
+    public const ROOT = "root";
+    public const ADMIN = "admin";
+    public const ENGINEER = "engineer";
+
+}
