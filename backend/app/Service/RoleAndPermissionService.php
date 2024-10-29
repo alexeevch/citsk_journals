7f5c1986c27a532dddfae3bf6468b74e5d0b0732
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-10-29 14:45:18 +0300

    feat: Интерфейс сервиса Ролей и прав

---
+++ backend/app/Service/RoleAndPermissionService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\DTO\Role\PermissionUpdateDTO;
+use App\DTO\Role\PermissonCreateDTO;
+use App\DTO\Role\RoleCreateDTO;
+use App\DTO\Role\RoleUpdateDTO;
+use App\Http\Resources\Auth\PermissionCollection;
+use App\Http\Resources\Auth\PermissionResource;
+use App\Http\Resources\Auth\RoleCollection;
+use App\Http\Resources\Auth\RoleResource;
+
+interface RoleAndPermissionService
+{
+    public function findRoleAll(): RoleCollection;
+
+    public function createRole(RoleCreateDTO $roleCreateDTO): RoleResource;
+
+    public function findRoleById(int $id): RoleResource;
+
+    public function updateRole(RoleUpdateDTO $roleUpdateDTO): RoleResource;
+
+    public function deleteRole(int $id): bool;
+
+    public function findPermissionAll(): PermissionCollection;
+
+    public function createPermission(PermissonCreateDTO $permissionCreateDTO): PermissionResource;
+
+    public function findPermissionById(int $id): PermissionResource;
+
+    public function updatePermission(PermissionUpdateDTO $permissionUpdateDTO): PermissionResource;
+
+    public function deletePermission(int $id): bool;
+}
