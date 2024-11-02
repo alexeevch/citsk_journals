96386702146afeece1a9b033a7352567395cb689
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2024-11-02 14:20:23 +0300

    fix(backend): Деструктуризация маршрутов

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,17 +1,33 @@
 <?php
 
-use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\IncidentController;
+use App\Constants;
+use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\IncidentController;
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("v1")->group(function () {
-    Route::post("/auth/login", [AuthController::class, "login"])->name("login");
+    /*    =======  Auth  ======= */
+    Route::prefix("auth")->group(function () {
+        Route::post("/login", [AuthController::class, "login"])->name("login");
+        Route::post("/register", [
+            UserController::class, "store"
+        ])->middleware(["auth:api, role:".Constants::ADMIN_ROLE."|".Constants::ROOT_ROLE]);
+    });
 
-    Route::group([
-        'middleware' => 'auth:api',
-    ],
-        function () {
-            Route::post("/auth/register", [AuthController::class, "register"]);
-            Route::apiResource("/incident", IncidentController::class);
-        });
+    /*    =======  User  ======= */
+    Route::middleware(["auth:api, role:".Constants::ADMIN_ROLE."|".Constants::ROOT_ROLE])->group(function () {
+        Route::get("/user", [UserController::class, "index"]);
+        Route::get("/user/{id}", [UserController::class, "show"]);
+        Route::patch("/user/{id}", [UserController::class, "update"]);
+        Route::put("/user/{id}", [UserController::class, "update"]);
+        Route::delete("/user/{id}", [UserController::class, "destroy"]);
+        Route::post("/user/assign-role", [UserController::class, "assignRoles"]);
+        Route::post("/user/assign-permission", [UserController::class, "assignPermissions"]);
+    })->middleware(['auth:api']);
+
+    /*    =======  Incident  ======= */
+    Route::middleware(["auth:api"])->group(function () {
+        Route::apiResource("/incident", IncidentController::class);
+    });
 });
