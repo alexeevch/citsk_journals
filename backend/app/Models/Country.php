a006f5f16adf7d6c17d6ef25299c74ca032ac717
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-26 20:07:46 +0300

    fix(backend): Уточнение таблицы в модели Country

--- backend/app/Models/Country.php
+++ backend/app/Models/Country.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -15,6 +16,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Country extends Model
 {
     use HasFactory;
+
+    protected $table = 'countries';
 
     public $timestamps = false;
 
