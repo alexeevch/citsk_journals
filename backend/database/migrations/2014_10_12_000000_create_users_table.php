c0a2eaf6d500ebba822e5dced18324c9f379a655
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-26 13:04:48 +0300

    feat(backend): Миграция на email в таблице user

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,15 +12,14 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->boolean("is_blocked")->default(false);
-            $table->string("login")->unique();
+            $table->string("email")->unique();
             $table->string("password");
             $table->string("first_name");
             $table->string("last_name");
             $table->string("patronymic")->nullable();
             $table->string("phone")->nullable();
-            $table->string("email")->unique();
             $table->string("post")->nullable();
+            $table->boolean("is_blocked")->default(false);
             $table->timestamps();
         });
     }
