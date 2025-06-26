50e79ef6f7588d4c0bd526e3bd59d5f04dbbc5b8
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-26 17:16:41 +0300

    feat(backend): Обновлен IncidentCreateDTO.php

--- backend/app/DTO/Incident/IncidentCreateDTO.php
+++ backend/app/DTO/Incident/IncidentCreateDTO.php
@@ -3,7 +3,6 @@
 namespace App\DTO\Incident;
 
 use App\DTO\Attacker\AttackerCreateDTO;
-use App\DTO\Infrastructure\InfrastructureCreateDTO;
 use DateTime;
 use WendellAdriel\ValidatedDTO\Casting\CarbonCast;
 use WendellAdriel\ValidatedDTO\Casting\DTOCast;
@@ -11,37 +10,39 @@ use WendellAdriel\ValidatedDTO\ValidatedDTO;
 
 class IncidentCreateDTO extends ValidatedDTO
 {
-    public AttackerCreateDTO $attacker;
-    public InfrastructureCreateDTO $infrastructure;
+    public ?AttackerCreateDTO $attacker;
+    public ?int $attacker_id;
+    public ?int $infrastructure_id;
     public array $type;
     public array $status;
     public ?string $description;
-    public DateTime $detection_time;
-    public DateTime $group_alert_time;
-    public DateTime $supervisor_alert_time;
+    public DateTime $detection_at;
+    public DateTime $group_notified_at;
+    public DateTime $supervisor_notified_at;
 
     protected function rules(): array
     {
         return [
-            'attacker'              => ['required', 'array'],
-            'infrastructure'        => ['required', 'array'],
-            'type'                  => ['required', 'array'],
-            'status'                => ['required', 'array'],
-            'description'           => ['string'],
-            'detection_datetime'        => ['required', 'date'],
-            'group_alert_datetime'      => ['required', 'date'],
-            'supervisor_alert_datetime' => ['required', 'date'],
+
+            'detection_at'           => ['required', 'date'],
+            'group_notified_at'      => ['required', 'date'],
+            'supervisor_notified_at' => ['required', 'date'],
+            'attacker_id'            => ['nullable', 'integer', 'exists:attackers, id'],
+            'attacker'               => ['nullable', 'array'],
+            'infrastructure_id'      => ['required', 'integer'],
+            'type'                   => ['required', 'array'],
+            'status'                 => ['required', 'array'],
+            'description'            => ['string'],
         ];
     }
 
     protected function casts(): array
     {
         return [
-            'attacker'              => new DTOCast(AttackerCreateDTO::class),
-            'infrastructure'        => new DTOCast(InfrastructureCreateDTO::class),
-            'detection_datetime'        => new CarbonCast(),
-            'group_alert_datetime'      => new CarbonCast(),
-            'supervisor_alert_datetime' => new CarbonCast(),
+            'attacker'               => new DTOCast(AttackerCreateDTO::class),
+            'detection_at'           => new CarbonCast(),
+            'group_notified_at'      => new CarbonCast(),
+            'supervisor_notified_at' => new CarbonCast(),
         ];
     }
 
