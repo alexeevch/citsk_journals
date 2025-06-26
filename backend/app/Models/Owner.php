c0eff669dbd31b068c82195142fb669dce91bd71
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-26 19:20:46 +0300

    feat(backend): PHP DOC для модели Owner

--- backend/app/Models/Owner.php
+++ backend/app/Models/Owner.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Eloquent;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int    $id
+ * @property string $name
+ * @property string $contact_email
+ * @property string $contact_phone
+ * @mixin Eloquent
+ */
 class Owner extends Model
 {
     use SoftDeletes;
