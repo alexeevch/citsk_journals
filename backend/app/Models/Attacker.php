3697b9b15701960534c7dde337c748a909ebfa13
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 10:16:06 +0300

    feat(backend): Проброшена связь Country в модель Attacker

--- backend/app/Models/Attacker.php
+++ backend/app/Models/Attacker.php
@@ -5,14 +5,15 @@ namespace App\Models;
 use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property int      $id
  * @property string   $ipv4
  * @property string   $description
- * @property string   $country
  * @property Incident $incident
+ * @property Country  $country
  * @mixin Eloquent
  */
 class Attacker extends Model
@@ -22,11 +23,16 @@ class Attacker extends Model
     protected $fillable = [
         "ipv4",
         "description",
-        "country",
+        "country_id",
     ];
 
     public function incident(): HasMany
     {
         return $this->hasMany(Incident::class, "attacker_id");
+    }
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, "country_id");
     }
 }
