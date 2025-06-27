c31b62df9baf8eceb163d191509a692c948daf1d
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 17:06:58 +0300

    feat(backend): Добавлен getAllPermissions() в модель User

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -72,4 +72,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function getAllPermissions(): \Illuminate\Support\Collection
+    {
+        return $this->getPermissionsViaRoles()->merge($this->permissions);
+    }
 }
