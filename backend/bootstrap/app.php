38fec12457d589f963c6ee93326096cb0a1a6a3d
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 15:50:17 +0300

    fix(backend): Переименован CustomExceptionHandler

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -38,7 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\CustomExceptionHandler::class
+    App\Exceptions\ApiExceptionHandler::class
 );
 
 /*
