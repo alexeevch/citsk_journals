23fed7831747325ffa5acea7749c6e466c8c668d
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 12:04:40 +0300

    feat(backend): Пагинация + фильтрация в репозитории Инцидента

--- backend/app/Repository/Incident/IncidentRepositoryImp.php
+++ backend/app/Repository/Incident/IncidentRepositoryImp.php
@@ -7,6 +7,7 @@ use App\DTO\Incident\IncidentUpdateDTO;
 use App\Models\Attacker;
 use App\Models\Incident;
 use App\Models\Infrastructure;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
 
 class IncidentRepositoryImp implements IncidentRepository
@@ -19,9 +20,9 @@ class IncidentRepositoryImp implements IncidentRepository
     {
         $incident = new Incident();
         $incident->description = $incidentCreateDTO->description;
-        $incident->detection_time = $incidentCreateDTO->detection_time;
-        $incident->group_alert_time = $incidentCreateDTO->group_alert_time;
-        $incident->supervisor_alert_time = $incidentCreateDTO->supervisor_alert_time;
+        $incident->detection_at = $incidentCreateDTO->detection_at;
+        $incident->group_notified_at = $incidentCreateDTO->group_notified_at;
+        $incident->supervisor_notified_at = $incidentCreateDTO->supervisor_notified_at;
         $incident->type = $incidentCreateDTO->type;
         $incident->status = $incidentCreateDTO->status;
 
@@ -74,6 +75,36 @@ class IncidentRepositoryImp implements IncidentRepository
     function findAll(): Collection
     {
         return Incident::all();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findAllPaginated(
+        int $perPage = 15,
+        int $page = 1,
+        ?array $filters = null,
+        ?string $sortField = 'id',
+        ?string $sortDirection = 'asc'
+    ): LengthAwarePaginator {
+        $query = Incident::query()->with([
+            'attacker',
+            'infrastructure',
+            'type',
+            'status'
+        ]);
+
+        if ($filters) {
+            foreach ($filters as $field => $value) {
+                if ($value !== null) {
+                    $query->where($field, $value);
+                }
+            }
+        }
+
+        $query->orderBy($sortField, $sortDirection);
+
+        return $query->paginate($perPage, ['*'], 'page', $page);
     }
 
     /**
