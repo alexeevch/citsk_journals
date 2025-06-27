38e58027a0e0fa09abb08a0d249bcbd46e3492e4
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 10:18:24 +0300

    fix(backend): Фикс IncidentResource.php

--- backend/app/Http/Resources/Incident/IncidentResource.php
+++ backend/app/Http/Resources/Incident/IncidentResource.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Resources\Incident;
 
+use App\Http\Resources\Attacker\AttackerResource;
+use App\Http\Resources\IncidentStatus\IncidentStatusResource;
+use App\Http\Resources\IncidentType\IncidentTypeResource;
+use App\Http\Resources\Infrastructure\InfrastructureResource;
 use App\Models\Incident;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -14,15 +18,15 @@ class IncidentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'                    => $this->id,
-            'attacker'              => $this->attacker,
-            'infrastructure'        => $this->infrastructure,
-            'type'                  => $this->type,
-            'status'                => $this->status,
-            'description'           => $this->description,
-            'detection_time'        => $this->detection_time,
-            'group_alert_time'      => $this->group_alert_time,
-            'supervisor_alert_time' => $this->supervisor_alert_time,
+            'id'                     => $this->id,
+            'attacker'               => new AttackerResource($this->attacker),
+            'infrastructure'         => new InfrastructureResource($this->infrastructure),
+            'type'                   => new IncidentTypeResource($this->type),
+            'status'                 => new IncidentStatusResource($this->status),
+            'description'            => $this->description,
+            'detection_at'           => $this->detection_at,
+            'group_notified_at'      => $this->group_notified_at,
+            'supervisor_notified_at' => $this->supervisor_notified_at,
         ];
     }
 }
