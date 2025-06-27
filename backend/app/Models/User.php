34e7f17736c47bf4c5e036b298024aafe5265cb8
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 16:32:15 +0300

    fix(backend): Обновлен phpdoc User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,22 +3,26 @@
 namespace App\Models;
 
 use App\Traits\HasCache;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 /**
- * @property int    $id
- * @property bool   $is_blocked
- * @property string $password
- * @property string $first_name
- * @property string $last_name
- * @property string $patronymic
- * @property string $post
- * @property string $email
- * @property string $phone
- * @property string $created_at
- * @property string $updated_at
+ * @property int                          $id
+ * @property bool                         $is_blocked
+ * @property string                       $password
+ * @property string                       $first_name
+ * @property string                       $last_name
+ * @property string                       $patronymic
+ * @property string                       $post
+ * @property string                       $email
+ * @property string                       $phone
+ * @property string                       $created_at
+ * @property string                       $updated_at
+ * @property-read Collection|Role[]       $roles
+ * @property-read Collection|Permission[] $permissions
+ * @property-read Collection|Permission[] $directPermissions
  */
 class User extends Authenticatable implements JWTSubject
 {
