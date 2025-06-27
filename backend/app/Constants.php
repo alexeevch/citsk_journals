30ce1c69e742612d0686ae53722272124bcde8ba
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-06-27 15:54:40 +0300

    fix(backend): Убрано лишнее из констант

--- backend/app/Constants.php
+++ backend/app/Constants.php
@@ -6,25 +6,6 @@ class Constants
 {
     /*
     |--------------------------------------------------------------------------
-    | MESSAGES
-    |--------------------------------------------------------------------------
-     */
-
-    public const USER_UNAUTHORIZED_MESSAGE = 'Unauthorized';
-    public const USER_UNAUTHENTICATED_MESSAGE = "Unauthenticated";
-    public const NOT_FOUND_MESSAGE = "Not found";
-    public const DUPLICATE_ENTRY_MESSAGE = "Duplicate entry";
-
-    /*
-    |--------------------------------------------------------------------------
-    | ERROR CODES
-    |--------------------------------------------------------------------------
-     */
-
-    public const DUPLUCATE_ENTRY_CODE = 1062;
-
-    /*
-    |--------------------------------------------------------------------------
     | ROLES
     |--------------------------------------------------------------------------
      */
