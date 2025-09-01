7e14980ccceb4f9ee334669c325610ab5b05db9b
alexeevch <2miklmiklmikl2@gmail.com>
2025-09-01 11:42:49 +0300

    fix(backend): Вынесены маршруты аутентификации из префикса версии api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\Api\v1\IncidentController;
 use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("v1")->group(function () {
-    Route::prefix("auth")->group(function () {
-        Route::post("/register", [AuthController::class, "register"])
-            ->middleware(['auth:sanctum', 'role:' . Constants::ADMIN_ROLE . '|' . Constants::ROOT_ROLE]);
-        Route::post("/login", [AuthController::class, "login"])->name("login");
-        Route::post("/logout", [AuthController::class, "logout"])->middleware("auth:sanctum");
-    });
+Route::prefix("auth")->group(function () {
+    Route::post("/register", [AuthController::class, "register"])
+        ->middleware(['auth:sanctum', 'role:' . Constants::ADMIN_ROLE . '|' . Constants::ROOT_ROLE]);
+    Route::post("/login", [AuthController::class, "login"])->name("login");
+    Route::post("/logout", [AuthController::class, "logout"])->middleware("auth:sanctum");
+});
 
+Route::prefix("v1")->group(function () {
     Route::middleware(['auth:sanctum', 'role:' . Constants::ADMIN_ROLE . '|' . Constants::ROOT_ROLE])->group(function () {
         Route::get("/users", [UserController::class, "index"]);
         Route::get("/user/{id}", [UserController::class, "show"]);
